fd35ec011067429f53d3723d858a0c247b19c9b3
Ravshan Mahmudov <2321772@gmail.com>
2021-08-20 22:23:26 +0300

    add modal and floor marcup

--- js/main.js
+++ js/main.js
@@ -3,12 +3,24 @@ $(document).ready(function () {
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var currentFlat = 2;
+  var flatPath = $(".flats path");
+  var viewFlatsButton = $(".view-flats")
+
 
   floorPath.on('mouseover', function () {
     currentFloor = $(this).attr("data-floor");
     floorPath.removeClass("current-floor");
     $(".counter").text(currentFloor);
   });
+
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+  viewFlatsButton.on('click', toggleModal);
+
+
   counterUp.on('click', function () {
     if (currentFloor < 18) {
       currentFloor++;
@@ -27,5 +39,13 @@ $(document).ready(function () {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
+  //   flatPath.on('mouseover', function () {
+  //   currentFlat = $(this).attr("data-flat");
+  //   flatPath.removeClass("current-flat");
+  //   $(".modal-counter").text(currentFlat);
+  // });
 
+  function toggleModal() {
+    modal.toggleClass("is-open");    
+  }
 });
